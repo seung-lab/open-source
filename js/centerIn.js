be7d04708032b604db9f12b8fb9713444a169805
William Silversmith <wms@mit.edu>
2014-02-11 22:43:06 -0500

    Updated centerIn with callbacks, percent offsets, and an updated margin calculation.

--- js/centerIn.js
+++ js/centerIn.js
@@ -4,7 +4,7 @@
  *
  * Author: William Silversmith
  * Affiliation: Seung Lab, Brain and Cognitive Sciences Dept., MIT
- * Date: August 2013
+ * Date: August 2013 - February 2014
  */
 ;(function($, undefined) {
 
@@ -16,6 +16,8 @@
 	 *
 	 * Required:
 	 *	 [0] selector: The element to center within
+	 *   [1] options or callback
+	 *   [2] callback (if [1] is options): Mostly useful for alwaysCenterIn
 	 *
 	 * Options:
 	 *	 direction: 'horizontal', 'vertical', 'both' (default)
@@ -24,15 +26,25 @@
 	 *
 	 * Returns: void
 	 */
-	$.fn.centerIn = function (selector, options) {
+	$.fn.centerIn = function (selector, options, callback) {
 		var elements = this;
 		options = options || {};
+
+        if (typeof(options) === 'function') {
+            callback = options;
+            options = {};
+        }
 
 		var direction = options.direction || $.fn.centerIn.defaults.direction;
 		var extraleft = options.left || 0;
 		var extratop = options.top || 0;
 
-		selector = $(selector).first();
+		if (selector) {
+			selector = $(selector).first();
+		}
+		else {
+			selector = elements.first().parent();
+		}
 
 		try {
 			if (!selector.css('position') || selector.css('position') === 'static') {
@@ -42,21 +54,21 @@
 		catch (e) {
 			// selector was something like window, document, html, or body
 			// which doesn't have a position attribute
-		};
+		}
 
 		var horizontal = function (element) {
-			var left = (selector.innerWidth() - element.innerWidth()) / 2;
-			left += extraleft;
+			var left = Math.round((selector.innerWidth() - element.outerWidth(false)) / 2);
+			left += translateDisplacement(selector, extraleft, 'width');
 			element.css('left', left + "px");
 		};
 
 		var vertical = function (element) {
-			var top = (selector.innerHeight() - element.innerHeight()) / 2;
-			top += extratop;
+			var top = Math.round((selector.innerHeight() - element.outerHeight(false)) / 2);
+			top += translateDisplacement(selector, extratop, 'height');
 			element.css('top', top + "px");
 		};
 
-		var centerfn = composeFunctions(horizontal, vertical, direction);
+		var centerfn = constructCenterFn(horizontal, vertical, callback, direction);
 
 		elements.each(function (index, element) {
 			element = $(element);
@@ -81,13 +93,15 @@
 		selector.centerIn.apply(selector, args);
 
 		var evt = 'resize.centerIn';
-		if (selector.attr('id')) {
-			evt += '.' + selector.attr('id');
-		}
+        if (selector.attr('id')) {
+            evt += '.' + selector.attr('id');
+        }
 
-		$(window).on(evt, function () {
+        $(window).on(evt, function () {
 			selector.centerIn.apply(selector, args);
 		});
+
+		return this;
 	 };
 
 	/* Defaults */
@@ -96,46 +110,57 @@
 		direction: 'both'
 	};
 
-	function composeFunctions(horizontal, vertical, direction) {
+    /* translateDisplacement
+     *
+     * Translates dimensionless units, pixel measures, and percent
+     * measures into px.
+     *
+     * Required: 
+     *   [0] selector: Container, relevant for percent measures
+     *   [1] value: Amount to displace. e.g. 5, "5px", or "5%"
+     *   [2] direction: 'width' or 'height'
+     * 
+     * Returns: px
+     */
+    function translateDisplacement(selector, value, direction) {
+        if (typeof(value) === 'number') {
+            return value;
+        }
+        else if (/px$/i.test(value)) {
+            return parseFloat(value.replace('px', ''), 10);
+        }
+        else if (/%$/.test(value)) {
+            var total = (direction === 'width')
+                ? $(selector).innerWidth()
+                : $(selector).innerHeight();
+
+            value = parseFloat(value.replace('%', ''), 10);
+            value /= 100;
+
+            return value * total;
+        }
+
+        return parseFloat(value, 10);
+    }
+
+	function constructCenterFn(horizontal, vertical, callback, direction) {
+        var fns = []
+
 		if (!direction || direction === 'both') {
-			return function (element) { 
-				vertical(element);
-				horizontal(element);
-			};
+			fns.push(vertical);
+            fns.push(horizontal);
 		}
 		else if (direction === 'horizontal') {
-			return function (element) { 
-				horizontal(element) 
-			};
+            fns.push(horizontal);
 		}
 		else if (direction === 'vertical') {
-			return function (element) {
-				vertical(element);
-			};
+            fns.push(vertical);
 		}
 
-		return function () {};
+        if (callback) {
+            fns.push(callback);
+        }
+
+		return Utils.compose(fns);
 	}
 })(jQuery);
-
-/* The MIT License (MIT)
-
-Copyright (c) 2014 Seung Lab, MIT
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
